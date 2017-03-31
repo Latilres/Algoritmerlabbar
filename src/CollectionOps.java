624dc80bce3a39be3b08f55c64faacecfcca62df
Aisserf <Aisserf@users.noreply.github.com>
2017-03-31 14:36:39 +0200

    Update CollectionOps.java

--- src/CollectionOps.java
+++ src/CollectionOps.java
@@ -66,5 +66,28 @@ public class CollectionOps {
     }
     
     // Put your code for filter here ...
+    public static <T> Collection<T>
+    filter(Predicate<T> p,Collection<T> c) 
+    {
+        // Determine the dynamic type of the collection
+        Class<? extends Collection> cls = c.getClass();
+        try {
+            // Create an object of the same dynamic type as c
+            Collection<T> result = (Collection<T>)cls.newInstance();
+            // type.cast(type.newInstance());
+            // Copy the elements and apply op to them
+            for ( T x : c ) {
+            	if((p.test(x))) {
+            		result.add(x);
+            	}
+            }
+            return result;   
+        }   
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
 
 }
